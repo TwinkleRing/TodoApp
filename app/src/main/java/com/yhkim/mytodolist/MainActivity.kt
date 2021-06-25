02e17bb05eca9ed2332613c64c00a480f56d4763
TwinkleRing <dlrmwl15@gmail.com>
2021-06-25 12:12:43 +0900

    06-25 Firebase Login&Logout 기능 추가

--- app/src/main/java/com/yhkim/mytodolist/MainActivity.kt
+++ app/src/main/java/com/yhkim/mytodolist/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.yhkim.mytodolist
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Paint
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.viewModels
@@ -15,11 +15,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.yhkim.mytodolist.databinding.ActivityMainBinding
 import com.yhkim.mytodolist.databinding.TodoItemViewBinding
 import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
+
+    val RC_SIGN_IN = 1000;
 
     private val viewModel : MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
@@ -28,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        // 로그인 안 됨
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            login()
+        }
 
         viewModel.addTodo(Todo("숙제"))
 
@@ -54,6 +64,57 @@ class MainActivity : AppCompatActivity() {
             (binding.RecyclerView.adapter as TodoAdapter).setData(it)
         })
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else { // 로그인 실패
+                finish() // 앱 꺼버린다.
+            }
+        }
+    }
+
+    fun login() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                login()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_log_out -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
 }
 
